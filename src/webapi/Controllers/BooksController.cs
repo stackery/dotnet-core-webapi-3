ed5a6f5cb0ab496313ddd6631ecc92b190386168
Chase Douglas <chase@stackery.io>
2020-05-04 16:03:32 -0700

    Port over .NET code!

---
+++ src/webapi/Controllers/BooksController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using webapi.Data;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private webapiContext db = new webapiContext();
+
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return db.Books
+                .Include(b => b.Author);
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/Books/5
+        public async Task<ActionResult> PutBook(int id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+        public async Task<ActionResult> PostBook(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Books.Add(book);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Books/5
+        public async Task<ActionResult> DeleteBook(int id)
+        {
+            Book book = await db.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.Books.Remove(book);
+            await db.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BookExists(int id)
+        {
+            return db.Books.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ src/webapi/Data/webapiContext.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using webapi.Models;
+
+namespace webapi.Data
+{
+    public class webapiContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        private Task<GetSecretValueResponse> secretResponseTask;
+    
+        public webapiContext()
+        {
+            AmazonSecretsManagerClient client = new AmazonSecretsManagerClient();
+
+            secretResponseTask = client.GetSecretValueAsync(new GetSecretValueRequest
+            {
+                SecretId = $"{Environment.GetEnvironmentVariable("SECRETS_NAMESPACE")}dbPassword"
+            });
+        }
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            GetSecretValueResponse response = secretResponseTask.Result;
+
+            String dbPassword = response.SecretString;
+            String dbAddress = Environment.GetEnvironmentVariable("DB_ADDRESS");
+            String dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+
+            optionsBuilder.UseSqlServer($"Data Source={dbAddress},{dbPort};Initial Catalog=books;User ID=root;Password={dbPassword}");
+        }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
